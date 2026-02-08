7af86a5394ae417f655bca530b9e2134a21d4f13
franzkhylong <franzkhylm.ong@gmail.com>
2026-02-08 20:28:24 +0800

    feat: update education entries

--- data/education.ts
+++ data/education.ts
@@ -9,9 +9,23 @@ export interface EducationItem {
 export const educationData: EducationItem[] = [
   {
     id: "1",
-    degree: "Bachelor of Science in Computer Science",
-    school: "University of Technology",
-    date: "2015 - 2019",
-    description: "Focus on Software Engineering and Artificial Intelligence. Graduated with Honors.",
+    degree: "Bachelor's Degree in Computer Engineering",
+    school: "Pamantasan ng Lungsod ng Maynila",
+    date: "2019 - 2023",
+    description: "Graduated as Magna Cum Laude",
+  },
+  {
+    id: "2",
+    degree: "DOST Merit Scholar",
+    school: "Department of Science and Technology",
+    date: "2019 - 2023",
+    description: "DOST-SEI RA 7687 Science and Technology Scholarship Program",
+  },
+  {
+    id: "3",
+    degree: "STEM Technology",
+    school: "Adamson University",
+    date: "2017 - 2019",
+    description: "Senior High School",
   },
 ];
